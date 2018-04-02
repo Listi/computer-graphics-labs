a9f5cb61fd013bf35813570bcccd2e5a9452427d
tasola <petter.tk@hotmail.com>
2018-04-02 20:08:15 +0200

    Solve 3.1

--- CG1/Supporting Files/skeleton.cpp
+++ CG1/Supporting Files/skeleton.cpp
@@ -14,6 +14,10 @@
 #include <vector>
 #include "SDLauxiliary.h"
 
+//Task 3
+#include <random>
+#include <iostream>
+
 using namespace std;
 using glm::vec3;
 
@@ -24,17 +28,26 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 SDL_Surface* screen;
 
+
 //vector<float> result( 10 );
 
-//Task 2.3
 
-vec3 red(1,0,0); // topLeft
-vec3 blue(0,0,1); // topRight
-vec3 green(0,1,0); // bottomLeft
-vec3 yellow(1,1,0); // bottomRight
-vector<vec3> leftSide( SCREEN_HEIGHT );
-vector<vec3> rightSide( SCREEN_HEIGHT );
-vector<vec3> horizontal ( SCREEN_WIDTH );
+//Task 2.3
+//
+//vec3 red(1,0,0); // topLeft
+//vec3 blue(0,0,1); // topRight
+//vec3 green(0,1,0); // bottomLeft
+//vec3 yellow(1,1,0); // bottomRight
+//vector<vec3> leftSide( SCREEN_HEIGHT );
+//vector<vec3> rightSide( SCREEN_HEIGHT );
+//vector<vec3> horizontal ( SCREEN_WIDTH );
+
+
+//Task 3
+
+vector<vec3> stars( 1000 );
+
+
 
 // --------------------------------------------------------
 // FUNCTION DECLARATIONS
@@ -71,24 +84,24 @@ void Draw();
 //}
 
 //Task 2.3
-
-std::vector<vec3> Interpolate( vec3 a, vec3 b, vector<vec3>& result ) {
-    
-    float xValue = (b.x - a.x)/(result.size()-1);
-    float yValue = (b.y - a.y)/(result.size()-1);
-    float zValue = (b.z - a.z)/(result.size()-1);
-    
-    result[0].x = a.x;
-    result[0].y = a.y;
-    result[0].z = a.z;
-    
-    for(int i=1; i<result.size(); ++i){
-        result[i].x = result[i-1].x+xValue;
-        result[i].y = result[i-1].y+yValue;
-        result[i].z = result[i-1].z+zValue;
-    }
-    return result;
-}
+//
+//std::vector<vec3> Interpolate( vec3 a, vec3 b, vector<vec3>& result ) {
+//
+//    float xValue = (b.x - a.x)/(result.size()-1);
+//    float yValue = (b.y - a.y)/(result.size()-1);
+//    float zValue = (b.z - a.z)/(result.size()-1);
+//
+//    result[0].x = a.x;
+//    result[0].y = a.y;
+//    result[0].z = a.z;
+//
+//    for(int i=1; i<result.size(); ++i){
+//        result[i].x = result[i-1].x+xValue;
+//        result[i].y = result[i-1].y+yValue;
+//        result[i].z = result[i-1].z+zValue;
+//    }
+//    return result;
+//}
 
 
 
@@ -97,6 +110,22 @@ std::vector<vec3> Interpolate( vec3 a, vec3 b, vector<vec3>& result ) {
 
 int main( int argc, char* argv[] )
 {
+    std::random_device rd;  //Will be used to obtain a seed for the random number engine
+    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+    std::uniform_real_distribution<> xyrand(-1.0, 1.0);
+    std::uniform_real_distribution<> zrand(0.0000001, 1.0);
+    
+    for(int star = 0; star<stars.size(); ++star){
+        
+        float randX = xyrand(gen);
+        float randY = xyrand(gen);
+        float randZ = zrand(gen);
+        
+        stars[star].x = randX;
+        stars[star].y = randY;
+        stars[star].z = randZ;
+    }
+    
     
     // Task 2.2 a)
     //
@@ -130,23 +159,54 @@ int main( int argc, char* argv[] )
     return 0;
 }
 
+
+// Task 2
+//void Draw()
+//{
+//
+////    Task 2.3
+////
+////    vector<vec3> downLeft = Interpolate( red, yellow, leftSide );
+////    vector<vec3> downRight = Interpolate( blue, green, rightSide );
+//
+//
+//    if( SDL_MUSTLOCK(screen) )
+//        SDL_LockSurface(screen);
+//    for( int y=0; y<SCREEN_HEIGHT; ++y )
+//    {
+////        Task 2.3
+////
+////        vector<vec3> right = Interpolate( downLeft[y], downRight[y], horizontal );
+//        for( int x=0; x<SCREEN_WIDTH; ++x )
+//        {
+////            Task 2.3
+////
+////            PutPixelSDL( screen, x, y, right[x] );
+//
+//            vec3 color(0,0,1);
+//            PutPixelSDL(screen, x, y, color);
+//        }
+//
+//    }
+//    if( SDL_MUSTLOCK(screen) )
+//        SDL_UnlockSurface(screen);
+//    SDL_UpdateRect( screen, 0, 0, 0, 0 );
+//}
+
 void Draw()
 {
-    vector<vec3> downLeft = Interpolate( red, yellow, leftSide );
-    vector<vec3> downRight = Interpolate( blue, green, rightSide );
-    
-    
+    SDL_FillRect( screen, 0, 0 );
     if( SDL_MUSTLOCK(screen) )
         SDL_LockSurface(screen);
-    for( int y=0; y<SCREEN_HEIGHT; ++y )
+    float halfHeight = SCREEN_HEIGHT / 2;
+    float halfWidth = SCREEN_WIDTH / 2;
+    for( size_t s=0; s<stars.size(); ++s )
     {
-        vector<vec3> right = Interpolate( downLeft[y], downRight[y], horizontal );
-        for( int x=0; x<SCREEN_WIDTH; ++x )
-        {
-            
-            PutPixelSDL( screen, x, y, right[x] );
-        }
-        
+        // Add code for projecting and drawing each star
+        int u = halfHeight * (stars[s].x / stars[s].z) + halfWidth;
+        int v = halfHeight * (stars[s].y / stars[s].z) + halfHeight;
+        vec3 color(1,1,1);
+        PutPixelSDL(screen, u, v, color);
     }
     if( SDL_MUSTLOCK(screen) )
         SDL_UnlockSurface(screen);
