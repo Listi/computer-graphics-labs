9c7934d7154b8830e19dacfec3a42336a722baf0
tasola <petter.tk@hotmail.com>
2018-04-26 14:16:14 +0200

    Add skeleton for 2.2 + Solve 2.2.1

---
+++ lab2/mathLab/CMakeFiles/mathLab.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mathLab.dir/main.cpp.o"
+  "mathLab.pdb"
+  "mathLab"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mathLab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab2/mathLab/myMatrix.h
@@ -0,0 +1,44 @@
+//Introductory matrix class
+
+#include <math.h>
+
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#else
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#endif
+
+
+namespace MyMathLab
+{
+
+class MyMatrix
+{
+	public:
+		MyMatrix(void);
+		~MyMatrix(void){;}
+
+		void loadIdentity(void);
+
+		void getGLModelviewMatrix(void);
+		void getGLProjectionMatrix(void);
+
+		void multiplyGLMatrix(void);
+		void setGLMatrix(void);
+
+	private:
+
+		void getGLMatrix(GLenum pname);
+
+		GLfloat myMatrix[16];
+};
+
+
+}
