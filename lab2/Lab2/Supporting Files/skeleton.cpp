e351c6c0c365f1c054663403ee69873769062002
tasola <petter.tk@hotmail.com>
2018-05-07 11:42:36 +0200

    Add Drawing Points

--- lab2/Lab2/Supporting Files/skeleton.cpp
+++ lab2/Lab2/Supporting Files/skeleton.cpp
@@ -12,17 +12,34 @@ using glm::mat3;
 // ----------------------------------------------------------------------------
 // GLOBAL VARIABLES
 
-const int SCREEN_WIDTH = 500;
-const int SCREEN_HEIGHT = 500;
+const int SCREEN_WIDTH = 110;
+const int SCREEN_HEIGHT = 110;
 SDL_Surface* screen;
 int t;
 vector<Triangle> triangles;
+vec3 cameraPos( 0, 0, -3.001 );
+mat3 R = glm::mat3(1.0f);
+float focalLength = 100;
 
 // ----------------------------------------------------------------------------
 // FUNCTIONS
 
 void Update();
 void Draw();
+
+void VertexShader( const vec3& v, ivec2& p ) {
+    //P'=(P-C)*R
+    //where {P'= projected3D = Position of the point in a coordinate system centered at the camera}
+    //      {P = v = Position of the point}
+    //      {C = cameraPos = Position of the camera}
+    vec3 projected3D = (v - cameraPos)*R;
+    // x = f*X/Z+W2 (here instead W/2 because of origin of the image)
+    // y = f*Y/Z+H2 (here instead H/2 because of origin of the image)
+    float projected2Dx = (focalLength*projected3D.x / projected3D.z+SCREEN_WIDTH/2);
+    float projected2Dy = (focalLength*projected3D.y / projected3D.z+SCREEN_HEIGHT/2);
+    p.x = projected2Dx;
+    p.y = projected2Dy;
+}
 
 int main( int argc, char* argv[] )
 {
@@ -101,8 +118,12 @@ void Draw()
 		vertices[0] = triangles[i].v0;
 		vertices[1] = triangles[i].v1;
 		vertices[2] = triangles[i].v2;
-
-		// Add drawing
+        for (int v=0; v<3; ++v){
+            ivec2 projPos;
+            VertexShader(vertices[v], projPos);
+            vec3 color(1,1,1);
+            PutPixelSDL(screen, projPos.x, projPos.y, color);
+        }
 	}
 	
 	if ( SDL_MUSTLOCK(screen) )
